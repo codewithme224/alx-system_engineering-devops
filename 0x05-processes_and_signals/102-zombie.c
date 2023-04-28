63d39fd971741a1326d593278bf7438718013f14
codewithme224 <codewithme224@gmail.com>
2023-04-28 05:16:29 -0700

    Zombie process

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -27,19 +27,16 @@ int main(void)
 {
 	int i;
 	pid_t pid;
-	int status;
 
 	for (i = 0; i < 5; i++)
 	{
 		pid = fork();
 		if (pid == 0)
 		{
-			exit(0);
+			printf("Zombie process created, PID: %d\n", getpid());
+			return (0);
 		}
-		else
-		{
-			printf("Zombie process created, PID: %d\n", pid);
-		}
+
 	}
 	infinite_while();
 	return (0);

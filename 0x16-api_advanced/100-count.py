60ea10c35ef0f89bd6ff086925ae939d0273b0a9
codewithme224 <codewithme224@gmail.com>
2023-08-08 20:00:28 -0700

    recursive function that queries the Reddit API

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -6,14 +6,40 @@ import requests
 def count_words(subreddit, word_list):
     """Queries the Reddit API, parses the title of all hot
     articles, and prints a sorted count of given keywords"""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'My-User-Agent'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        for i in range(len(response.json().get('data').get('children'))):
-            for word in word_list:
-                if word.lower() in response.json().get('data').get('children')
-                [i].get('data').get('title').lower():
-                    print(word.lower())
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
+                            .format(subreddit),
+                            params={"after": after},
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code != 200:
+        return None
+
+    info = sub_info.json()
+
+    hot_l = [child.get("data").get("title")
+             for child in info
+             .get("data")
+             .get("children")]
+    if not hot_l:
+        return None
+
+    word_list = list(dict.fromkeys(word_list))
+
+    if word_count == {}:
+        word_count = {word: 0 for word in word_list}
+
+    for title in hot_l:
+        split_words = title.split(' ')
+        for word in word_list:
+            for s_word in split_words:
+                if s_word.lower() == word.lower():
+                    word_count[word] += 1
+
+    if not info.get("data").get("after"):
+        sorted_counts = sorted(word_count.items(), key=lambda kv: kv[0])
+        sorted_counts = sorted(word_count.items(),
+                               key=lambda kv: kv[1], reverse=True)
+        [print('{}: {}'.format(k, v)) for k, v in sorted_counts if v != 0]
     else:
-        print(None)
+        return count_words(subreddit, word_list, word_count,
+                           info.get("data").get("after"))

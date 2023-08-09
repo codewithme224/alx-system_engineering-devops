2584baaf5fd1014905e152bad17125e2a2600e31
codewithme224 <codewithme224@gmail.com>
2023-08-08 19:47:49 -0700

    recursive function that queries the Reddit API

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,18 +6,22 @@ import requests
 def recurse(subreddit, hot_list=[], count=0, after=None):
     """Queries the Reddit API and returns a list containing the
     titles of all hot articles for a given subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'My-User-Agent'}
-    params = {'count': count, 'after': after}
-    response = requests.get(url, headers=headers, params=params,
+    response = requests.get("https://www.reddit.com/r/{}/hot.json"
+                            .format(subreddit),
+                            params={"count": count, "after": after},
+                            headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
-    if response.status_code == 200:
-        for i in range(len(response.json().get('data').get('children'))):
-            hot_list.append(response.json().get('data').get('children')
-                            [i].get('data').get('title'))
-        after = response.json().get('data').get('after')
-        if after is None:
-            return hot_list
-        return recurse(subreddit, hot_list, after, params.get(cont))
-    else:
+    if response.status_code >= 400:
         return None
+
+    after = hot_list + [child.get("data").get("title")
+                        for child in response.json()
+                        .get("data")
+                        .get("children")]
+
+    info = response.json()
+    if not info.get("data").get("after"):
+        return after
+
+    return recurse(subreddit, after, info.get("data").get("count"),
+                   info.get("data").get("after"))
